405687e3b15918d6c298f63ad6b29fb3340b7d69
mudasar187 <ahmmud16@hotmail.com>
2018-09-18 12:35:46 +0200

    created jwt

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://ahmmud16:ahmmud16@ds155352.mlab.com:55352/devconnect'
+    mongoURI: 'mongodb://ahmmud16:ahmmud16@ds155352.mlab.com:55352/devconnect',
+    secretOrKey: 'secret'
 }
