1521d14c99e0f731aee8940751d8752f6492782c
mudasar187 <ahmmud16@hotmail.com>
2018-09-19 10:45:11 +0200

    added delete route to delete a user and user´s profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -226,4 +226,63 @@ router.post('/education', passport.authenticate('jwt', { session: false }), (req
         })
 });
 
+
+// @route DELETE api/profile/experince/:exp_id
+// @dec Delete experince from profile
+// @access Private
+router.delete('/experience/:exp_id', passport.authenticate('jwt', { session: false }), (req,res) => {
+    
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+           // Get remove index
+           const removeIndex = profile.experience
+            .map(item => item.id)
+            .indexOf(req.params.exp_id);
+            
+            // Splice out array
+            profile.experience.splice(removeIndex, 1);
+
+            // Save
+            profile.save().then(profile => res.json(profile));
+        
+        }).catch(err => res.status(404).json(err));
+});
+
+
+// @route DELETE api/profile/education/:edu_id
+// @dec Delete education from profile
+// @access Private
+router.delete('/education/:edu_id', passport.authenticate('jwt', { session: false }), (req,res) => {
+    
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+           // Get remove index
+           const removeIndex = profile.education
+            .map(item => item.id)
+            .indexOf(req.params.exp_id);
+            
+            // Splice out array
+            profile.education.splice(removeIndex, 1);
+
+            // Save
+            profile.save().then(profile => res.json(profile));
+        
+        }).catch(err => res.status(404).json(err));
+});
+
+
+// @route   DELETE api/profile
+// @desc    Delete user and profile
+// @access  Private
+router.delete('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+      
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+        User.findOneAndRemove({ _id: req.user.id }).then(() =>
+          res.json({ success: true })
+        );
+      });
+    }
+  );
+
+
 module.exports = router;
