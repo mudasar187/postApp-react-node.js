1c7a854b11be5259418805be9ecfb18650fbb4df
mudasar187 <ahmmud16@hotmail.com>
2018-09-26 18:33:05 +0200

    added devtools extensions

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -15,7 +15,7 @@ class App extends Component {
     render() {
       return (
         <Provider store={store}>
-        <Router>
+        <BrowserRouter>
           <div className="App">
             <NavBar />
               <Route exact path="/" component={Landing} />
@@ -25,7 +25,7 @@ class App extends Component {
               </div>
             <Footer />
         </div>
-        </Router>
+        </BrowserRouter>
         </Provider>
       );
     }
