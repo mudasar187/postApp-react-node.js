cc8e90b11724888cdc1535b38e43007f456e7feb
mudasar187 <ahmmud16@hotmail.com>
2018-09-27 14:30:28 +0200

    added login reducer

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,11 +1,14 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 // Register
 export const registerUser = (userData, history) => dispatch => {
   axios
     .post("http://localhost:3000/api/users/register", userData)
-    .then(res => history.push('/login'))
+    .then(res => history.push("/login"))
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
@@ -13,3 +16,39 @@ export const registerUser = (userData, history) => dispatch => {
       })
     );
 };
+
+// Login - Get User Token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("http://localhost:3000/api/users/register", userData)
+    .then(res => {
+      // Save to localStorage
+      const { token } = res.data;
+      
+      // Set token to localStorage
+      localStorage.setItem("jwtToken", token);
+      
+      // Set token to auth header
+      setAuthToken(token);
+
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Set logged in user
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+}
