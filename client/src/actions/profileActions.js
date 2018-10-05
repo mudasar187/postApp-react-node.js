f305d73e646d3231eb079f271081f0dde9defe92
Mudasar Ahmad <ahmmud16@hotmail.com>
2018-10-05 19:43:37 +0200

    added links in create-profile page and delete account funcionality

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS } from "./types";
+import {
+  GET_PROFILE,
+  PROFILE_LOADING,
+  CLEAR_CURRENT_PROFILE,
+  GET_ERRORS,
+  SET_CURRENT_USER
+} from "./types";
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -21,11 +27,31 @@ export const getCurrentProfile = () => dispatch => {
     );
 };
 
+// Delete account & profile
+export const deleteAccount = () => dispatch => {
+  if (window.confirm("Are your sure? This can NOT be undone")) {
+    axios
+      .delete("/api/profile")
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
+};
+
 // Create Profile
 export const createProfile = (profileData, history) => dispatch => {
   axios
-    .post('/api/profile', profileData)
-    .then(res => history.push('/dashboard'))
+    .post("/api/profile", profileData)
+    .then(res => history.push("/dashboard"))
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
