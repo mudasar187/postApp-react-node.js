50f642d5d0301a748f24541c89e1804debb66ea3
Mudasar Ahmad <ahmmud16@hotmail.com>
2018-10-08 14:14:12 +0200

    finished commentfeed and commentitem component, prepare for heroku deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://ahmmud16:ahmmud16@ds155352.mlab.com:55352/devconnect",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
