3c736ed94f7b80621cabb956dc5f652debeaf266
Mudasar Ahmad <ahmmud16@hotmail.com>
2018-10-08 17:57:20 +0200

    changed config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -10,38 +10,42 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        options: { presets: ['env'], plugins: ["transform-class-properties"] }
+        loader: "babel-loader",
+        options: { presets: ["env"], plugins: ["transform-class-properties"] }
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-        loader: 'url-loader?limit=100000'
+        loader: "url-loader?limit=100000"
       }
     ]
   },
-  resolve: { extensions: ['*', '.js', '.jsx'] },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
     path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
     filename: "bundle.js"
   },
-  devtool: 'cheap-module-eval-source-map',
+  devtool: "cheap-module-eval-source-map",
   devServer: {
     contentBase: path.join(__dirname, "public/"),
     proxy: {
-      "/api/*":{
-          target:"http://localhost:3000/",
-          secure:"true"
-      },
+      "/api/*": {
+        target: "http://localhost:3000/",
+        secure: "true"
+      }
     },
     port: 4000,
     publicPath: "http://localhost:4000/dist/",
     hotOnly: true,
     historyApiFallback: true
   },
-  plugins: [ new webpack.HotModuleReplacementPlugin(), new HtmlWebpackPlugin() ]
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "./index.html"
+    })
+  ]
 };
