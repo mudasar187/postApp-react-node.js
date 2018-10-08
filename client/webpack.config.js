822c8b1ca72d5924c064b2ff7b045fe4b659412b
Mudasar Ahmad <ahmmud16@hotmail.com>
2018-10-08 18:10:40 +0200

    changed config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -45,7 +45,8 @@ module.exports = {
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      filename: "./index.html"
+      filename: 'index.html',
+      template: './public/index.html'
     })
   ]
 };
