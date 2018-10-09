a525573eff06730f370ac126bd6938cde01a9c39
Mudasar Ahmad <ahmmud16@hotmail.com>
2018-10-09 10:32:35 +0200

    fixed github links

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -93,7 +93,6 @@ class App extends Component {
                   <Route path="" component={NotFound} />
                 </Switch>
               </div>
-
               <Footer />
             </Switch>
           </div>
