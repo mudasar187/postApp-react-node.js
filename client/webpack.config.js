039f300c06cacbf2aa3cb3d3429fe5eaeadb34cb
Mudasar Ahmad <ahmmud16@hotmail.com>
2018-10-09 11:24:39 +0200

    problem routing on heroku

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
       }
     ]
   },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
+  resolve: { extensions: [".js", ".jsx"] },
   output: {
     path: path.resolve(__dirname, "dist/"),
     filename: "bundle.js"
